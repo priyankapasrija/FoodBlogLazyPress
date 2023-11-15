18d4f95c19518024374e36bc22380f303a9ceff9
priyankapasrija <104025698+priyankapasrija@users.noreply.github.com>
2023-11-15 14:57:23 +0100

    added tailwind and nextui

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+//
+//
+// function App() {
+//
 
-function App() {
-
-  return (
-  
-  
-  )
-}
-
-export default App
+//   return (
+//
+//
+//   )
+// }
+//
+// export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+import { NextUIProvider } from "@nextui-org/react";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <React.StrictMode>
+      <NextUIProvider>
+        <App />
+      </NextUIProvider>
+    </React.StrictMode>
+  </BrowserRouter>
+);
