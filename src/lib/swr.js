d7476a3f6139765040f973fa5c734ea3e34550a1
gjlander <garrett.jlander@gmail.com>
2023-11-22 19:50:55 +0100

    using swr on hero banner-updates when I click on next slide but waits until then

---
+++ src/lib/swr.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+import useSWR from "swr";
+
+const backend = "http://localhost:24601";
+
+const lazyPress = axios.create({
+    baseURL: backend,
+});
+
+const fetcher = (url) => lazyPress.get(url).then((res) => res.data);
+
+const useFoodBlog = () => {
+    const { data, error, isLoading } = useSWR(
+        `/blogs/6558a1e8b9710288a16552e3`,
+        fetcher
+    );
+    return {
+        foodBlog: data,
+        isLoading,
+        isError: error,
+    };
+};
+
+export { useFoodBlog };
