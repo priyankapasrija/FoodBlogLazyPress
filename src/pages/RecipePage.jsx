6d4e99f7e7000edae06a7c476bd248fc9a80f6ab
Mochi McCoy <hi@mochibun.me>
2023-11-30 01:32:49 +0100

    Minor pre-work setup

--- src/pages/RecipePage.jsx
+++ src/pages/RecipePage.jsx
@@ -19,13 +19,10 @@ export default function RecipePage() {
   // const [theData, setTheData] = useState(null);
   const { recipePage, isLoading } = useRecipePage(pageId);
   document.title = `Food Blog`;
-  
 
-
-    
-    if (!recipePage) {
-        return <div>Recipe not found</div>;
-      }
+  if (!recipePage) {
+    return <div>Recipe not found</div>;
+  }
 
   // useEffect(() => {
   //     const fetchData = async () => {
@@ -55,11 +52,10 @@ export default function RecipePage() {
   if (!isLoading) document.title = `${recipePage.title} | Food Blog`;
   return (
     <>
-      <Breadcrumbs className="p-" style={{ borderBottom: "1px solid black" }}>
+      <Breadcrumbs className="py-2" style={{ borderBottom: "1px solid black" }}>
         <BreadcrumbItem className="ml-[50px]" style={{ fontSize: "1rem" }}>
-          {" "}
           <Link style={{ textDecoration: "none" }} to="/" relative="path">
-            Home{" "}
+            Home
           </Link>
         </BreadcrumbItem>
         <BreadcrumbItem>Recipe</BreadcrumbItem>
@@ -92,7 +88,7 @@ export default function RecipePage() {
       <div
         className="flex flex-col justify-center gap-5 items-start "
         style={{
-          width: "100vw",
+          // width: "100vw",
           fontFamily: "Montserrat",
           fontSize: "1.2rem",
         }}
@@ -111,33 +107,11 @@ export default function RecipePage() {
               );
             })}
           </ol>
-          {/* <p className="border border-black p-4 borderShadow">
-                        Ground lean (7% fat) beef,
-                        <br />
-                        Large egg,
-                        <br />
-                        Minced onion, <br />
-                        Fine dried bread crumbs, <br />
-                        Worcestershire sauce <br />
-                        Cloves garlic, peeled and minced
-                        <br />
-                        Salt <br />
-                        Pepper
-                        <br />
-                        Hamburger buns (4 in. wide), split <br />
-                        Mayonnaise <br />
-                        Ketchup (1st occurrence)
-                        <br />
-                        Ketchup (2nd occurrence) <br />
-                        Iceberg lettuce leaves, rinsed <br />
-                        Firm-ripe tomato, cored and thinly sliced <br />
-                        Red onion, thin slices <br />
-                    </p> */}
         </div>
 
         <div
-          className="flex flex-col justify-start items-start vl p-5"
-          style={{ width: "30vw" }}
+          className="flex flex-col justify-start items-start p-5"
+          // style={{ width: "30vw" }}
         >
           <h1 className="h1">How to Prep?</h1>
           {recipePage?.steps.map((step, i) => {
@@ -156,81 +130,10 @@ export default function RecipePage() {
               </div>
             );
           })}
-          {/* <h3
-                        className="h3"
-                        style={{
-                            fontFamily: "Barlow Condensed",
-                            fontSize: "2rem",
-                        }}
-                    >
-                        Step 1
-                    </h3>
-                    <p>
-                        In a bowl, mix ground beef, egg, onion, bread crumbs,
-                        Worcestershire, garlic, 1/2 teaspoon salt, and 1/5
-                        teaspoon pepper until well blended. Divide mixture into
-                        four equal portions and shape each into a patty about 4
-                        inches wide.
-                    </p>
-                    <h1
-                        className="h3"
-                        style={{
-                            fontFamily: "Barlow Condensed",
-                            fontSize: "2rem",
-                        }}
-                    >
-                        Step 2
-                    </h1>
-                    <p>
-                        In a bowl, mix ground beef, egg, onion, bread crumbs,
-                        Worcestershire, garlic, 1/2 teaspoon salt, and 1/5
-                        teaspoon pepper until well blended. Divide mixture into
-                        four equal portions and shape each into a patty about 4
-                        inches wide.
-                    </p>
-                    <h1
-                        className="h3"
-                        style={{
-                            fontFamily: "Barlow Condensed",
-                            fontSize: "2rem",
-                        }}
-                    >
-                        Step 3
-                    </h1>
-                    <p>
-                        In a bowl, mix ground beef, egg, onion, bread crumbs,
-                        Worcestershire, garlic, 1/2 teaspoon salt, and 1/5
-                        teaspoon pepper until well blended. Divide mixture into
-                        four equal portions and shape each into a patty about 4
-                        inches wide.
-                    </p>
-                    <h1
-                        className="h3"
-                        style={{
-                            fontFamily: "Barlow Condensed",
-                            fontSize: "2rem",
-                        }}
-                    >
-                        Step 4
-                    </h1>
-                    <p>
-                        In a bowl, mix ground beef, egg, onion, bread crumbs,
-                        Worcestershire, garlic, 1/2 teaspoon salt, and 1/5
-                        teaspoon pepper until well blended. Divide mixture into
-                        four equal portions and shape each into a patty about 4
-                        inches wide.
-                    </p> */}
         </div>
-        
-        <div>
-          {/* <Comments /> */}
-          <Button>Share on Twitter</Button>
-          <Button>Share on Facebook</Button>
-          <Button>Share on Instagram</Button>
+        <div className="w-8/12 mx-auto mb-20">
+          <CommentsSection page={recipePage} />
         </div>
-				<div className="w-full">
-					<CommentsSection page={recipePage} />
-				</div>
       </div>
     </>
   );
