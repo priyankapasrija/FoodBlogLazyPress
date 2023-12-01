d5191af2ec8a03d7451de87e2232dcc70e81b15e
priyankapasrija <104025698+priyankapasrija@users.noreply.github.com>
2023-12-01 09:57:44 +0100

    addded new package

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -1,6 +1,10 @@
+import { InlineWidget } from "react-calendly";
 export default function ContactUs() {
-return(
+  return (
     <>
+      <div>
+        <InlineWidget url="https://calendly.com/your_scheduling_page" />
+      </div>
     </>
-)
+  );
 }
