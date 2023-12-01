22ba0f84e189d2d9b4eb76d5987e7f751335bcdc
Harun Kohen Erdeniz <110021233+kohkohshine@users.noreply.github.com>
2023-12-01 01:05:14 +0100

    tags were added to utils for mochi

--- src/utils/category.js
+++ src/utils/category.js
@@ -1,54 +1,40 @@
 const ctgryColor = (category) => {
-    if (category === "Dessert") {
-        return "bg-slate-500";
-    }
-    if (category === "Beef") {
-        return "bg-orange-500";
-    }
-    if (category === "Vegetarian") {
-        return "bg-red-700";
-    }
-    if (category === "Chicken") {
-        return "bg-blue-500";
-    }
-    if (category === "Seafood") {
-        return "bg-violet-400";
-    }
-    if (category === "Pork") {
-        return "bg-lime-500";
-    }
-    if (category === "Side") {
-        return "bg-fuchsia-700";
-    }
-    if (category === "Lamb") {
-        return "bg-yellow-400";
-    }
-    if (category === "Miscellaneous") {
-        return "bg-amber-700";
-    }
-    if (category === "Pasta") {
-        return "bg-pink-600";
-    }
-    if (category === "Breakfast") {
-        return "bg-yellow-900";
-    }
-    if (category === "Starter") {
-        return "bg-cyan-200";
-    }
-    if (category === "Vegan") {
-        return "bg-lime-700";
-    }
-    if (category === "Cool") {
-        return "bg-violet-800";
-    }
-    if (category === "Goat") {
-        return "bg-violet-950";
-    }
-    if (category === "Misc") {
-        return "bg-amber-900";
-    } else {
-        return "bg-slate-700";
+    switch (category) {
+        case "Dessert":
+            return "bg-slate-500";
+        case "Beef":
+            return "bg-orange-500";
+        case "Vegetarian":
+            return "bg-red-700";
+        case "Chicken":
+            return "bg-blue-500";
+        case "Seafood":
+            return "bg-violet-400";
+        case "Pork":
+            return "bg-lime-500";
+        case "Side":
+            return "bg-fuchsia-700";
+        case "Lamb":
+            return "bg-yellow-400";
+        case "Miscellaneous":
+            return "bg-amber-700";
+        case "Pasta":
+            return "bg-pink-600";
+        case "Breakfast":
+            return "bg-yellow-900";
+        case "Starter":
+            return "bg-cyan-200";
+        case "Vegan":
+            return "bg-lime-700";
+        case "Cool":
+            return "bg-violet-800";
+        case "Goat":
+            return "bg-violet-950";
+        case "Misc":
+            return "bg-amber-900";
+        default:
+            return "bg-slate-700";
     }
 };
 
-export {ctgryColor};
+export { ctgryColor };
