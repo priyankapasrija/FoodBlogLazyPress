340fc4ba11f55b9b8acd22bf47a912facf50fc3c
Harun Kohen Erdeniz <110021233+kohkohshine@users.noreply.github.com>
2023-12-01 14:47:25 +0100

    homepage styling is done

---
+++ src/components/RecipeQuote.jsx
@@ -0,0 +1,17 @@
+export default function RecipeQuote() {
+return(
+<div className="bg-dots h-[70vh] items-center flex justify-center" style={{borderBottom:'1px solid black'}}>
+<p className='bg-[#fced82] text-black mb-2 uppercase p-4 border border-black boxShadow'
+ style={{
+     fontFamily: "Baryton",
+     fontSize: "6rem",
+     lineHeight:1,
+     width:'80vw',
+     height:'60vh'
+ }}>
+    Discover adorable recipes from across the world▼</p>
+</div>
+)
+
+
+}
