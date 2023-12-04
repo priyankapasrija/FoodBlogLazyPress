2349e6752f7000e7e9d6ac4da1b8edc876e7786c
priyankapasrija <104025698+priyankapasrija@users.noreply.github.com>
2023-12-04 11:45:42 +0100

    added calendly

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -1,15 +1,4 @@
 import { InlineWidget } from "react-calendly";
 export default function ContactUs() {
-  return (
-    // <div
-    //   className="calendly-inline-widget"
-    //   data-url="https://calendly.com/priyankapasrija22"
-    //   style={{ width: "350px", height: "700px" }}
-    // ></div>
-    <InlineWidget
-      url="https://calendly.com/priyankapasrija22"
-      style={{ width: "350px", height: "700px" }}
-    />
-    // {/* <script type="text/javascript" src="https://assets.calendly.com/assets/external/widget.js" async></script> */}
-  );
+  return <InlineWidget url="https://calendly.com/lazypress" />;
 }
