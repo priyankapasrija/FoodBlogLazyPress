276c882a8a026fe03c492dee92396272c214cc1a
Harun Kohen Erdeniz <110021233+kohkohshine@users.noreply.github.com>
2023-12-05 10:21:06 +0100

    search bar is aligned for 14inch screen

---
+++ src/components/Comment.jsx
@@ -0,0 +1,11 @@
+export default function Comment () {
+
+    return (
+
+        <div>
+        <h4>userName:comment.userName</h4>
+        <p>comment.text</p>
+      </div>
+
+    )
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import HeroBanner from "../components/HeroBanner";
+import CardSection from "../components/CardSection";
+import RecipeQuote from "../components/RecipeQuote";
+
+export default function Home(/*{data}*/) {
+   
+   
+   
+
+    return (
+        <>
+       <HeroBanner  />
+       <RecipeQuote />
+       <CardSection />  
+        </>
+    );
+}
